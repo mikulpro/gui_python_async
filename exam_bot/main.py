cc5cb1d750b1c23d6a29c3f4bf78d57aebdf7c61
Prokop Mikulasek <prokop12@volny.cz>
2023-08-17 11:23:23 +0200

    Presun bot_core do main na dobu docasnou

--- exam_bot/main.py
+++ exam_bot/main.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 from tkinter import simpledialog, filedialog
-from bot_core import BotCore
 import asyncio
 import threading
 
@@ -8,6 +7,40 @@ import threading
 TRESHOLD = 5
 CANVAS_WIDTH = 1700
 CANVAS_HEIGHT = 900
+
+import discord                                                          # Discord API
+import requests                                                         # knihovna pro HTTP requesty
+from bs4 import BeautifulSoup                                           # knihovna pro parsovani HTML
+from discord.ext.commands import Bot, has_permissions                   # knihovna pro Discord boty
+
+class BotCore:
+    _instance = None
+
+    def __new__(cls, in_token):
+        if cls._instance is None:
+            cls._instance = super(BotCore, cls).__new__(cls)
+        return cls._instance
+
+    def __init__(self, in_token):
+        self.token = in_token
+
+        intents = discord.Intents.all()                                         # Intents jsou "zapouzrena" nastaveni discord opravneni pro bota
+        intents.typing = True                                                   # Urcuje, jestli bot muze psat zpravy
+        intents.presences = True                                                # Urcuje, jestli muze bot videt, kdyz se nekdo pripoji nebo odpoji
+
+        bot = Bot(command_prefix='/', intents=intents)
+
+        @bot.event
+        async def on_ready():
+            print(f'{bot.user} has connected to Discord!')
+
+        bot.run(self.token)
+
+    async def setup_hook(self):
+        global rectangles
+        for item in rectangles:
+            if item.is_active:
+                await self.load_extension(item.associated_file)
 
 class SnappableRectangle:
     
