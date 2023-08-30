4717221496300c5134c3c674ab76dd81ac9be31d
Prokop Mikulasek <prokop12@volny.cz>
2023-08-30 13:59:14 +0200

    GUI progress

--- exam_bot/interactive_user_interface.py
+++ exam_bot/interactive_user_interface.py
@@ -2,33 +2,65 @@ from tkinter import simpledialog, filedialog, Canvas, Button, Tk, N, NW, W, SW, 
 from random import choice, uniform, randint
 
 # konstanty pro snapovani bloku
-CANVAS_WIDTH = 1000
-CANVAS_HEIGHT = 1000
+CANVAS_WIDTH = 900
+CANVAS_HEIGHT = 900
 SNAP_TRESHOLD = 5
-RECTANGLE_SIDE_SIZE = 50
-EDGE_ZONE_SIZE = 70
+RECTANGLE_SIDE_SIZE = 100
+CORE_SIDE_SIZE = RECTANGLE_SIDE_SIZE # zatim blbne, kdyz je jiny nez RECTANGLE_SIDE_SIZE
+EDGE_ZONE_SIZE = RECTANGLE_SIDE_SIZE+10
+FONT_SIZE = 28
+FONT_FAMILY =  "Helvetica"
+# FONT_FAMILY jsou "Arial", "Calibri", "Comic Sans MS", "Courier New", "Georgia", "Helvetica", "Impact", "Lucida Console", "Lucida Sans Unicode", "Palatino Linotype", "Tahoma", "Times New Roman", "Trebuchet MS", "Verdana"
 
-class SnappableRectangle:
+class _SnappableRectangle:
     
-    def __init__(self, canvas, x1, y1, x2, y2, fill, input_root):
-        self.root = input_root
-        self.canvas = canvas
-        self.rect = canvas.create_rectangle(x1, y1, x2, y2, fill=fill, tags="rectangle")
-        self.connected_rectangles = []
-        self.delete_button = Button(canvas, text="Delete", command=self.root.delete_rectangle)
+    def __init__(self, canvas, x1, y1, x2, y2, fill, input_root, show_delete=True):
         self.associated_file = None
+        self.canvas = canvas
+        self.connected_rectangles = []
+        self.delete_button = None
+        self.delete_button_id = None
         self.is_active = False
         self.prev_x = 0
         self.prev_y = 0
+        self.rect = canvas.create_rectangle(x1, y1, x2, y2, fill=fill, tags="rectangle")
+        self.root = input_root
+        self.window_id = None
+        
+        if show_delete:
+            self.delete_button = Button(master=self.canvas, text="Delete", command=lambda: self.root.delete_rectangle(self))
+            self.window_id = self.canvas.create_window((x1 + (x2-x1)/2, y1 + (y2-y1)/2), window=self.delete_button, anchor=CENTER)
+
+        if not show_delete:
+            self.delete_button = Button(master=self.canvas, text="", height=0, width=0, state='disabled', command=lambda: self.root.delete_rectangle(self))
+            self.window_id = self.canvas.create_window((x1 + (x2-x1)/2, y1 + (y2-y1)/2), window=self.delete_button, anchor=CENTER)
+
+    def delete(self):
+        # Delete rectangle from canvas
+        self.canvas.delete(self.rect)
+        
+        # Delete associated delete button
+        if self.delete_button:
+            self.delete_button.destroy()
+            self.canvas.delete(self.window_id)
+
+        # Remove from root's rectangle list
+        if self in self.root.rectangles:
+            self.root.rectangles.remove(self)
+        
+        # Remove from delete_buttons dictionary
+        if self.rect in self.root.delete_buttons:
+            del self.root.delete_buttons[self.rect]
 
 class Tk_extended(Tk):
-    def __init__(self):
-        super().__init__()
-        self.rectangles = []
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)  # Assuming you're extending a parent class. If not, remove this line.
+        self.canvas = None
         self.delete_buttons = {}
+        self.rectangles = []
+        self.rect_to_button_id_dict = {}
         self.selected_rectangle = None
         self.token = None
-        self.canvas = None
 
     def delete_rectangle(self, rectangle_obj):
         rectangle_obj.delete()    
@@ -70,19 +102,21 @@ class Tk_extended(Tk):
                     y_distance_bottom = abs(bbox[3] - other_bbox[1])
 
                     if x_distance_left < SNAP_TRESHOLD and 0 < y_distance_bottom < (bbox[3] - bbox[1]) + (other_bbox[3] - other_bbox[1]):
-                        return other_rect.rect
+                        return other_rect
                     elif x_distance_right < SNAP_TRESHOLD and 0 < y_distance_top < (bbox[3] - bbox[1]) + (other_bbox[3] - other_bbox[1]):
-                        return other_rect.rect
+                        return other_rect
                     elif y_distance_top < SNAP_TRESHOLD and 0 < x_distance_right < (bbox[2] - bbox[0]) + (other_bbox[2] - other_bbox[0]):
-                        return other_rect.rect
+                        return other_rect
                     elif y_distance_bottom < SNAP_TRESHOLD and 0 < x_distance_left < (bbox[2] - bbox[0]) + (other_bbox[2] - other_bbox[0]):
-                        return other_rect.rect
-
+                        return other_rect
         return None
 
-    def align_rectangles(self, rect1, rect2, x, y):
-        bbox1 = self.canvas.bbox(rect1)
-        bbox2 = self.canvas.bbox(rect2)
+    def align_rectangles(self, rect1, rect2, x, y):      
+        bbox1 = self.canvas.bbox(rect1.rect)
+        bbox2 = self.canvas.bbox(rect2.rect)
+
+        if bbox1 is None or bbox2 is None:
+            return
 
         x_distance_left = abs(bbox1[0] - bbox2[2])
         x_distance_right = abs(bbox1[2] - bbox2[0])
@@ -102,57 +136,62 @@ class Tk_extended(Tk):
             x_offset = 0
             y_offset = bbox2[1] - bbox1[3]
 
-        self.canvas.move(rect1, x + x_offset, y + y_offset)
+        self.canvas.move(rect1.rect, x + x_offset, y + y_offset)
         self.move_delete_button(rect1, x + x_offset, y + y_offset)
 
-    def spawn_rectangle(self):
+        self.canvas.move(rect2.rect, x, y)
+        self.move_delete_button(rect2, x, y)
+
+    def spawn_rectangle(self, is_core=False):
         x1 = randint((0 + EDGE_ZONE_SIZE), (CANVAS_WIDTH - EDGE_ZONE_SIZE))
         y1 = randint((0 + EDGE_ZONE_SIZE), (CANVAS_HEIGHT - EDGE_ZONE_SIZE))
-        x2, y2 = x1+RECTANGLE_SIDE_SIZE, y1+RECTANGLE_SIDE_SIZE
 
-        new_rectangle = SnappableRectangle(self.canvas, x1, y1, x2, y2, "green", self)
-        self.delete_buttons[new_rectangle.rect] = new_rectangle.delete_button
+        if is_core:
+            x2, y2 = x1+CORE_SIDE_SIZE, y1+CORE_SIDE_SIZE
+            new_rectangle = _SnappableRectangle(canvas=self.canvas, x1=x1, y1=y1, x2=x2, y2=y2, fill="red", input_root=self, show_delete=False)
+        else:
+            x2, y2 = x1+RECTANGLE_SIDE_SIZE, y1+RECTANGLE_SIDE_SIZE
+            new_rectangle = _SnappableRectangle(canvas=self.canvas, x1=x1, y1=y1, x2=x2, y2=y2, fill="grey", input_root=self, show_delete=True)
+        self.delete_buttons[new_rectangle.rect] = new_rectangle.window_id
+        self.rect_to_button_id_dict[new_rectangle] = new_rectangle.window_id
         self.rectangles.append(new_rectangle)
-        self.canvas.create_window((x1, y1), window=new_rectangle.delete_button, anchor=CENTER)
 
         #TODO: priradit k rectanglu konkretni soubor i s ikonkou
 
-    def move_delete_button(self, rectangle_obj, dx, dy):
-        print(rectangle_obj.rect)
-        print(self.delete_buttons.keys())
-        if rectangle_obj.rect in self.delete_buttons:
-            bbox = self.canvas.bbox(rectangle_obj.rect)
-            self.canvas.coords(self.delete_buttons[rectangle_obj.rect], (bbox[0] + dx, bbox[1] + dy))
+        return new_rectangle
+    
+    def move_delete_button(self, rect, dx, dy):
+        button_id = self.get_button_id_for_rectangle(rect)
+        if button_id:
+            self.canvas.move(button_id, dx, dy)
+        else:
+            print(f"Error: No button ID found for rectangle {rect.rect}")
+              
+    def get_button_id_for_rectangle(self, rect):
+        return self.rect_to_button_id_dict[rect]
 
     def get_rectangle_from_id(self, canvas_id):
         for rect in self.rectangles:
             if rect.rect == canvas_id:
                 return rect
         return None
-
-    def move_delete_button(self, rectangle_obj, dx, dy):
-        bbox = self.canvas.bbox(rectangle_obj.rect)
-        self.canvas.coords(self.delete_buttons[rectangle_obj.rect], (bbox[0] + dx, bbox[1] + dy))    
     
 ##############################################################################################################
 
 def tkinter_extended_start():
-    root = Tk_extended()
-    root.title("Cog Control")
+    TKe = Tk_extended()
+    TKe.title("Cog Control")
+    TKe.canvas = Canvas(TKe, width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
+    TKe.canvas.pack()
 
-    root.canvas = Canvas(root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
-    root.canvas.pack()
+    TKe.spawn_rectangle(is_core=True)
+    TKe.canvas.bind("<ButtonPress-1>", TKe.on_click)
+    TKe.canvas.bind("<B1-Motion>", TKe.on_drag)
 
-    core_rectangle = SnappableRectangle(root.canvas, 100, 100, 200, 200, "blue", root)
-    root.rectangles.append(core_rectangle)
-
-    root.canvas.bind("<ButtonPress-1>", root.on_click)
-    root.canvas.bind("<B1-Motion>", root.on_drag)
-
-    spawn_button = Button(root, text="Spawn Rectangle", command=root.spawn_rectangle)
+    spawn_button = Button(TKe, text="Spawn Rectangle", command=TKe.spawn_rectangle, font=(FONT_FAMILY, FONT_SIZE))
     spawn_button.pack()
 
-    root.mainloop()
+    TKe.mainloop()
 
 if __name__ == "__main__":
     tkinter_extended_start()
